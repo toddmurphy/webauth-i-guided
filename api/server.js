8a2c5051138d46075d737e9a2cfbb1433035766e
toddmurphy <toddmurphy93@gmail.com>
2020-01-27 14:34:09 -0400

    working on auth

--- api/server.js
+++ api/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const bcrypt = require('bcryptjs');
 const apiRouter = require('./api-router.js');
 const configureMiddleware = require('./configure-middleware.js');
 
